edfa0aec5aeaadc7edb251614fdcaf699b5484ed
Ankush Ranapure <ar2653@njit.edu>
2024-04-18 19:49:09 -0400

    Updated quals

--- src/main/java/org/checkerframework/checker/dividebyzero/qual/Bottom.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/qual/Bottom.java
@@ -4,7 +4,7 @@ import org.checkerframework.framework.qual.SubtypeOf;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
-@SubtypeOf(Top.class)
+@SubtypeOf({Zero.class, NonZero.class})
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface Bottom {
 }
--- src/main/java/org/checkerframework/checker/dividebyzero/qual/NonZero.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/qual/NonZero.java
@@ -4,7 +4,7 @@ import org.checkerframework.framework.qual.SubtypeOf;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
-@SubtypeOf({Top.class, Bottom.class})
+@SubtypeOf({Top.class})
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 public @interface NonZero {
 }
